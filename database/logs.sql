99795a0ea1aff529538e12e529c823864458c153
egor <codeegor@gmail.com>
2022-12-08 15:49:01 +0200

    optimized incrementing code

--- database/logs.sql
+++ database/logs.sql
@@ -1,13 +1,13 @@
-CREATE TABLE test.logs (
-	id BIGINT auto_increment NULL,
-	ip_address varchar(100) NOT NULL,
-	user_agent varchar(255) NOT NULL,
-	view_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NULL,
-	image_id VARCHAR(100) NOT NULL,
-	view_count BIGINT UNSIGNED NOT NULL,
-	CONSTRAINT logs_PK PRIMARY KEY (id)
+CREATE TABLE `logs` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ip_address` varchar(100) NOT NULL,
+  `user_agent` varchar(255) NOT NULL,
+  `view_date` timestamp NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `image_id` varchar(100) NOT NULL,
+  `view_count` bigint(20) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `logs_image_id_IDX` (`image_id`,`user_agent`,`ip_address`) USING HASH
 )
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
-
--- src/Entities/Log.php
+++ src/Entities/Log.php
@@ -13,21 +13,9 @@ class Log extends Entity {
   }
 
   public function increment(string $id, string $ip, string $agent): void {
-    $present = $this->db->fetchOne(
-      "SELECT id, view_count as count FROM {$this->table} WHERE ip_address = :ip and user_agent = :agent and image_id = :id",
+    $this->db->exec(
+      "INSERT INTO {$this->table} SET ip_address = :ip, user_agent = :agent, image_id = :id, view_count= 1 ON DUPLICATE KEY UPDATE view_count= {$this->table}.view_count + 1",
       ['ip' => $ip, 'agent' => $agent, 'id' => $id]
     );
-
-    if ($present) {
-      $this->db->exec(
-        "UPDATE {$this->table} SET view_count = :count WHERE id = :id",
-        ['count' => (int)$present->count + 1, 'id' => $present->id]
-      );
-    } else {
-      $this->db->exec(
-        "INSERT INTO {$this->table} SET ip_address = :ip, user_agent = :agent, image_id = :id, view_count= 1",
-        ['ip' => $ip, 'agent' => $agent, 'id' => $id]
-      );
-    }
   }
 }
